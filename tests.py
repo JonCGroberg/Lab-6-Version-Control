b222c0bf1645f3a9a8ad176ac7586841a0b3bd1d
Jonathan Groberg <jongroberg@gmail.com>
2024-03-16 18:26:50 -0400

    updated tests to show failure error

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from main import encode, decode
 
 if __name__ == '__main__':
-    assert encode("00009962") == "33332295"
-    assert decode("33332295") == "00009962"
+    assert encode(
+        "00009962") == "33332295", f'encode("00009962") should return "33332295" but returns {encode("00009962")}'
+    assert decode(
+        "33332295") == "00009962", f'decode("33332295") should return "00009962" but returns {decode("33332295")}'
